f3e30f2b180cb4b1b43af880b015244aba2aadcb
Josh Merrick <moshjerrick@gmail.com>
2024-07-05 19:11:46 +0000

    added html to template file

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -37,5 +37,7 @@ def number_route(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     return render_template('5-number.html', n=n)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
