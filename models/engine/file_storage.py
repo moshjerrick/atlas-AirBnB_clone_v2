d996330b6366e17dc8bed13f5cbed643c17c6029
Josh Merrick <moshjerrick@gmail.com>
2024-07-08 18:38:03 +0000

    updated file_storage with def_close callling reload

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -65,3 +65,6 @@ class FileStorage:
             if key in self.__objects:
                 del self.__objects[key]
 
+    def close(self):
+        """call reload() method for deserializing the JSON file to objects"""
+        self.reload()
